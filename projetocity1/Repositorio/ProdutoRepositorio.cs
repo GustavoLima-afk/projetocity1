ab8f30e7e052e579d4ad9790a6eb176ae2714d67
Gustavo <slima.gusta@gmail.com>
2025-03-27 15:07:57 -0300

    adicionando os codigos do MYSQL no UsuarioRepositorio e ProdutoRepositorio

--- projetocity1/Repositorio/ProdutoRepositorio.cs
+++ projetocity1/Repositorio/ProdutoRepositorio.cs
@@ -1,9 +1,29 @@
 ﻿using MySql.Data.MySqlClient;
- using System.Data;
+using System.Data;
 
 namespace projetocity1.Repositorio
 {
     public class ProdutoRepositorio
     {
+        //Declaração privada e somente de leitura
+        private readonly string _connectionString;
+
+        //metodo de configuração chamando a string de conexão no appsettings.json 
+        public UsuarioRepositorio(IConfiguration configuration) => _connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // metodo adicionar usuario(esse metodo aparece na controller)
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.CreateCommand();
+                cmd.CommandText = "INSERT INTO Usuarios (Nome, Email, Senha) VALUES (@Nome, @Email, @Senha)";
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
     }
 }
