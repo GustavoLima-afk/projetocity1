a633bae844c738fa29516055dd88f189a7520d8f
GustavoLimaafk <slima.gusta.com>
2025-03-27 20:35:40 -0300

    Criando os controladores UsuarioController e Pordutocontroller

--- projetocity1/Controllers/ProdutoController.cs
+++ projetocity1/Controllers/ProdutoController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using projetocity1.Models;
+using projetocity1.Repositorio;
 
 namespace projetocity1.Controllers
 {
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+        /*construtor*/
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+        public IActionResult Cadastro()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Cadastro(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _produtoRepositorio.AdicionarProduto(produto);
+                return RedirectToAction("Cadastro");
+            }
+            return View(produto);
+        }
     }
 }
+
